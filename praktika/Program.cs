1d18632c091a3d91501878c86f831d2935540c5f
Senanmemmedov1990 <memmedov8665341@gmail.com>
2023-10-30 02:17:29 +0400

    first lesson

---
+++ praktika/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace praktika
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            int a = 1; 
+            int b = 10;
+
+            int sum = 0;
+            for (int i = a;i <= b; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+          
+        }
+        static void Main(string[] args)
+        {
+           int a = 1, b = 5;
+            int sum = 0;
+            for(int i = a;i <= b;i++)
+            {
+                sum = 21 / i;
+                Console.WriteLine(sum);
+                
+            }
+
+        }
+    }
+}
